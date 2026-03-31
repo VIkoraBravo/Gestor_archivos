c70e3d072d30b42fd55ae4af65d0e017708874d5
VikoraBravo <nicolasomgbravo@gmail.com>
2026-03-31 12:27:16 -0300

    Automatización de base de datos y admin

--- almacen/apps.py
+++ almacen/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class AlmacenConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'almacen'
+
+    def ready(self):
+        # Conectamos una función que se ejecute justo después de las migraciones
+        post_migrate.connect(create_admin, sender=self)
+
+def create_admin(sender, **kwargs):
+    from django.contrib.auth.models import User
+    import os
+    
+    # Creamos un superusuario por defecto si no existe ninguno
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@ejemplo.com', 'admin1234')
+        print("Superusuario 'admin' creado exitosamente con clave 'admin1234'")

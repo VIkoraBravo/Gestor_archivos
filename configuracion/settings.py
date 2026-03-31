d7cd2e064731e2f726b1a070961dce0a978a207d
VikoraBravo <nicolasomgbravo@gmail.com>
2026-03-31 16:29:36 -0300

    Guardado de archivos

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'almacen', 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 

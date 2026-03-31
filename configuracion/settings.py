a559a98a478bbb401a6837da437c809cfde20fb6
VikoraBravo <nicolasomgbravo@gmail.com>
2026-03-31 16:12:57 -0300

    Guardado de archivos

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'almacen', 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
